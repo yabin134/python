39fef27ed41d2b84fc6f034f3daf3811d937242f
yabin134 <tonytessy3@gmail.com>
2024-05-23 11:00:58 +0530

    hello

---
+++ #time.py
@@ -0,0 +1,14 @@
+#time
+import time
+a=time.time()
+print(a)
+# print(time.ctime())
+# print(time.ctime(time.time()))
+# print(time.ctime()) #current time
+
+
+
+#time.sleep()
+# for i in range(1,6):
+#     print(i)
+#     time.sleep(3)
